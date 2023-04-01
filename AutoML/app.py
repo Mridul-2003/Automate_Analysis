dce039d4dca8d7774fbfa954ebb9e66bde5f6091
Mridul-2003 <110475111+Mridul-2003@users.noreply.github.com>
2023-04-01 15:43:11 +0530

    visualise

--- AutoML/app.py
+++ AutoML/app.py
@@ -31,6 +31,38 @@ def main():
         </div>
         """
         st.markdown(html_temp,unsafe_allow_html=True)
+    if rad == "Visualize":
+        file_upload=st.sidebar.file_uploader(" ",type=["csv"])
+        st.sidebar.image(image_loan,use_column_width=True) 
+        chart_select = st.sidebar.selectbox(
+            label = "select the chart type",
+            options=['ScatterPlots','Lineplots','Histogram','Boxplot']
+        )
+        html_temp = """
+        <div style="background-color:red;padding:10px">
+        <h2 style="color:white;text-align:center;">Automatic Machine Learning </h2>
+        </div>
+        """
+        st.markdown(html_temp,unsafe_allow_html=True)
+        st.sidebar.title("Upload Input csv file : ")
+        st.subheader('1.Datasets')
+        st.write("""
+    # Automated Machine Learning
+    """)
+
+
+        if file_upload is not None:
+            df = pd.read_csv(file_upload)
+            st.write(df)
+            numeric_columns = list(df.select_dtypes(['float','int']).columns)
+            if chart_select == "ScatterPlots":
+                st.sidebar.subheader("ScatterPlot Settings")
+                x_values = st.sidebar.selectbox('X axis',options=numeric_columns)
+                y_values = st.sidebar.selectbox('Y axis',options=numeric_columns)
+                plot = px.scatter(data_frame =df, x=x_values,y=y_values)
+                st.plotly_chart(plot)
+        else:
+            st.info('Awaiting for CSV file to be uploaded.')
     if rad == "Analysis":
          
         file_upload=st.sidebar.file_uploader(" ",type=["csv"])
